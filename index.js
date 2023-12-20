25c2961e445f68baa487b381f2feeb71497f16d5
Sudesh Bandara <sudeshnbandara@gmail.com>
2023-12-20 23:20:16 +0530

    new-update

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const jsonServer = require("json-server"); // importing json-server library
 const server = jsonServer.create();
-const router = jsonServer.router("db.json");
+// const router = jsonServer.router("db.json");
 
-const server2 = jsonServer.create();
-const router2 = jsonServer.router("discount.json");
+// const server2 = jsonServer.create();
+// const router2 = jsonServer.router("discount.json");
+
+router = [
+    jsonServer.router("db.json"),
+    jsonServer.router("discount.json")
+]
 
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 8080; //  chose port from here like 8080, 3001
@@ -12,9 +17,9 @@ server.use(middlewares);
 server.use(router);
 server.listen(port);
 
-server2.use(middlewares);
-server2.use(router2);
-server2.listen(port);
+// server2.use(middlewares);
+// server2.use(router2);
+// server2.listen(port);
 
 
 
